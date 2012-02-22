6a3e31628ebef3257c4e887b7ee18371ad1ec61f
Antares Yee <antaresyee@gmail.com>
2012-02-22 16:56:19 -0500

    testing Main

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ public class Main {
 		TivooSystem s = new TivooSystem();
 		s.loadCal("http://www.cs.duke.edu/courses/cps108/current/assign/02_tivoo/data/dukecal.xml");
 		s.loadCal("http://www.cs.duke.edu/courses/cps108/current/assign/02_tivoo/data/googlecal.xml");
+		//s.filterByKeyword("meet with");
 		s.outputSummaryAndDetailsPages(System.getProperty("user.home") + "/Desktop");
 		//System.out.println(s.myEvents.toString());
-		s.filterByKeyword("meet with");
 		//DateTime dt=new DateTime("2011-11-16T13:30:00");
 		//s.filterByTime(dt);
 		//s.outputSummaryAndDetailsPages("/Desktop");
