882fe228c7279b5a3b250f833cd1783be9b88648
Glenn Rivkees <glenn.rivkees@gmail.com>
2012-02-24 12:59:09 -0500

    add parsing exception

---
+++ src/xmlParse/ParsingException.java
@@ -0,0 +1,14 @@
+package xmlParse;
+
+public class ParsingException extends RuntimeException {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ParsingException(String args) {
+		super(args);
+	}
+
+}
