345e5af92fb5d0df62deac25c24dfe0d1901cf3f
Gang Song <gang.song@duke.edu>
2012-03-02 13:01:03 -0500

    Clean up Event class

--- src/process/Event.java
+++ src/process/Event.java
@@ -9,7 +9,7 @@ import java.util.Set;
 import org.joda.time.DateTime;
 import org.joda.time.Interval;
 
-public class Event implements Comparable<Event> {
+public class Event{
 	
 	private Map<String, ArrayList<String>> myMap;
 
@@ -87,74 +87,6 @@ public class Event implements Comparable<Event> {
 	public Interval getInterval() {
 		return myInterval;
 	}
-
-	public String getStartYear() {
-		return Integer.toString(myStartTime.getYear());
-	}
-
-	public String getStartMonth() {
-		return Integer.toString(myStartTime.getMonthOfYear());
-	}
-
-	public String getStartDayOfMonth() {
-		return Integer.toString(myStartTime.getDayOfMonth());
-	}
-
-	public String getStartDayOfWeek() {
-		return Integer.toString(myStartTime.getDayOfWeek());
-	}
-
-	public String getStartHourOfDay() {
-		return Integer.toString(myStartTime.getHourOfDay());
-	}
-
-	public String getStartMinuteOfHour() {
-		return Integer.toString(myStartTime.getMinuteOfHour());
-	}
-
-	public String getEndYear() {
-		return Integer.toString(myEndTime.getYear());
-	}
-
-	public String getEndMonth() {
-		return Integer.toString(myEndTime.getMonthOfYear());
-	}
-
-	public String getEndDayOfWeek() {
-		return Integer.toString(myEndTime.getDayOfWeek());
-	}
-
-	public String getEndDayOfMonth() {
-		return Integer.toString(myEndTime.getDayOfMonth());
-	}
-
-	public String getEndHourOfDay() {
-		return Integer.toString(myEndTime.getHourOfDay());
-	}
-
-	public String getEndMinuteOfHour() {
-		return Integer.toString(myEndTime.getMinuteOfHour());
-	}
-
-    @Override
-    public int compareTo(Event other) {
-        if (myMap.get("myName").equals(other.myMap.get("myName"))&& myInterval.equals(other.myInterval) && myMap.get("myLocation").equals(other.myMap.get("myLocation"))) {
-            return 0;
-        }
-        
-        //compare by time
-        if (getStartTime().compareTo(other.getStartTime()) < 0)
-            return -1;
-        else if (getStartTime().compareTo(other.getStartTime()) > 0)
-            return 1;
-        else {
-            if (getEndTime().compareTo(other.getEndTime()) < 0)
-                return -1;
-            else if (getEndTime().compareTo(other.getEndTime()) > 0)
-                return 1;
-        }
-        return 0;
-    }
 
 
 }
